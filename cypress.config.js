43ede5d30cf55106c338d339b65db03e4d2d9054
fabiana.silva <fabiana.silva@eemovel.com>
2023-05-18 18:36:25 -0300

    Third test

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://magento.softwaretestingboard.com/",
-    
+    viewportWidth: 1336,
+    viewportHeight: 960
     
     },
     
